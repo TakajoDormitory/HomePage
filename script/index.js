0ac32b33270278d551dd320c7958d4965eecbc2f
shebang-sh <c19yamamoto@gmail.com>
2020-10-22 02:16:36 +0900

    お知らせ機能実装!

--- script/index.js
+++ script/index.js
@@ -1,30 +1,26 @@
-// お知らせ
-
-$.getJSON('json/notice.json')
-    .done(function (data) {
-        console.log("GetData!");
+$(function(){
+    $.getJSON("json/notice.json",function(data){
         console.log(data);
-        $.each(data,function(value){
-            $("#notice .list").append('<li><dl><dt>'+value.date+'</dt><dd><a href="html/notice/'+value.href+'.html">'+value.title+'</a></dd></dl></li>');
-        });
+        for(let i=data.length-1;i>=0;i--){
+        $("#list").append($('<li><dl><dt>'+data[i].date+'</dt><dd><a href="html/notice/'+data[i].href+'.html">'+data[i].title+'</a></dd></dl></li>'));
+    }
 });
 
-// topボタン
-$(function() {
-    let pagetop = $('#page_top');   
-    pagetop.hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 150) {
-            pagetop.fadeIn();
-        }
-        else {
-            pagetop.fadeOut();
-        }
-    });
-    pagetop.click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500); //0.5秒でトップへ移動
-        return false;
-    });
+// トップへ戻るボタン
+let pagetop = $('#page_top');   
+pagetop.hide();
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 150) {
+        pagetop.fadeIn();
+    }
+    else {
+        pagetop.fadeOut();
+    }
+});
+pagetop.click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500); //0.5秒でトップへ移動
+    return false;
+});
 });
