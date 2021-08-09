37c1651c1b7f9b3bc9e2166f7e6d96f72bb85cea
shebang-sh <c19yamamoto@gmail.com>
2021-08-09 16:13:21 +0900

    change the method of loading short movie.

--- script/index.js
+++ script/index.js
@@ -38,4 +38,16 @@ $(function () {
             $('#header').css({ 'background-color': 'rgba(0,0,0,0)' });
         }
     });
+
+    // 画像が押されたらyoutubeのiframeを読み込む
+    $('.youtube').each(function () {
+        var iframe = $(this).children('iframe');
+        var url = iframe.attr('data-src');
+        var id = url.match(/[\/?=]([a-zA-Z0-9_-]{11})[&\?]?/)[1];
+        iframe.before('<img id="inner-video" src="http://img.youtube.com/vi/' + id + '/mqdefault.jpg" />').remove();
+        $(this).on('click', function () {
+            $(this).after('<div class="youtube"><iframe id="inner-video" src="https://www.youtube.com/embed/' + id + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>').remove();
+            $('#movie h3').remove();
+        });
+    });
 });
