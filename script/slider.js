6c16020c2f69aa66d71771adfc93dc7138ce9914
shebang-sh <c19yamamoto@gmail.com>
2021-08-20 13:09:40 +0900

    Define slide function

--- script/slider.js
+++ script/slider.js
@@ -1,124 +1,104 @@
-// 表示する画像の配列を作成
-let imgList;
-// htmlNameはcommon.jsで定義済み
-if (htmlName[htmlName.length - 2] == 'en') {
-	imgList = [
-		"../img/OverView.jpg",
-		"../img/EntranceCeremony.JPG",
-		"../img/Firework.JPG",
-		"../img/soccer.jpg",
-	];
-}
-else {
-	imgList = [
-		"img/OverView.jpg",
-		"img/EntranceCeremony.JPG",
-		"img/Firework.JPG",
-		"img/soccer.jpg",
-	];
-}
-
-// 画像とナビの要素を自動で追加
-for (let i = 0; i < imgList.length; i++) {
-	// li要素を取得
-	let slide = document.createElement("li");
-	// li要素の中に画像タグを埋め込む
-	if (window.innerWidth < 1450) slide.innerHTML = "<img width=100% src='" + imgList[i] + "'>";
-	else slide.innerHTML = "<img width='800px' height='450px' src='" + imgList[i] + "'>";
-	// li要素をクラス名「slider-inner」の子要素として追加
-	document.getElementsByClassName("slider-inner")[0].appendChild(slide);
-
-	// li要素を取得
-	let nav = document.createElement("li");
-	// プロパティ「data-nav-index」に数値を割り振る
-	nav.setAttribute("data-nav-index", i);
-	// li要素をクラス名「nav」の子要素として追加
-	document.getElementsByClassName("nav")[0].appendChild(nav);
-}
-
-// スライドの数を取得(処理のために-1する)
-let length = imgList.length - 1;
-// クラス名「imageSlide」に画像の1枚の要素を格納
-let imageSlide = document.getElementsByClassName("slider-inner")[0].getElementsByTagName("li");
-// クラス名「dotNavigation」にドットナビの1つの要素を格納
-let dotNavigation = document.getElementsByClassName("nav")[0].getElementsByTagName("li");
-// 「現在○○枚目のスライドを表示している」というインデックス番号を格納する変数
-let nowIndex = 0;
-// 現在表示されている画像とドットナビにクラス名を付ける
-imageSlide[nowIndex].classList.add("show");
-dotNavigation[nowIndex].classList.add("current");
-// スライドがアニメーション中か判断するフラグ
-let isChanging = false;
-// スライドのsetTimeoutを管理するタイマー
-let slideTimer;
-// スライド切り替え時に呼び出す関数
-function sliderSlide(val) {
-	if (isChanging === true) {
-		return false;
+$(document).ready(function () {
+	// 表示する画像の配列を作成
+	let imgList;
+	// htmlNameはcommon.jsで定義済み
+	if (htmlName[htmlName.length - 2] == 'en') {
+		imgList = [
+			"../img/OverView.jpg",
+			"../img/EntranceCeremony.JPG",
+			"../img/Firework.JPG",
+			"../img/soccer.jpg",
+		];
 	}
-	isChanging = true;
-	// 現在表示している画像とナビからクラス名を削除
-	imageSlide[nowIndex].classList.remove("show");
-	dotNavigation[nowIndex].classList.remove("current");
-	nowIndex = val;
-	// 次に表示するスライドとナビにカレントクラスを設定
+	else {
+		imgList = [
+			"img/OverView.jpg",
+			"img/EntranceCeremony.JPG",
+			"img/Firework.JPG",
+			"img/soccer.jpg",
+		];
+	}
+
+	// 画像とナビの要素を自動で追加
+	for (let i = 0; i < imgList.length; i++) {
+		// li要素を取得
+		let slide = document.createElement("li");
+		// li要素の中に画像タグを埋め込む
+		if (window.innerWidth < 1450) slide.innerHTML = "<img width=100% src='" + imgList[i] + "'>";
+		else slide.innerHTML = "<img width='800px' height='450px' src='" + imgList[i] + "'>";
+		// li要素をクラス名「slider-inner」の子要素として追加
+		document.getElementsByClassName("slider-inner")[0].appendChild(slide);
+
+		// li要素を取得
+		let nav = document.createElement("li");
+		// プロパティ「data-nav-index」に数値を割り振る
+		nav.setAttribute("data-nav-index", i);
+		// li要素をクラス名「nav」の子要素として追加
+		document.getElementsByClassName("nav")[0].appendChild(nav);
+	}
+
+	// スライドの数を取得(処理のために-1する)
+	let length = imgList.length - 1;
+	// クラス名「imageSlide」に画像の1枚の要素を格納
+	let imageSlide = document.getElementsByClassName("slider-inner")[0].getElementsByTagName("li");
+	// クラス名「dotNavigation」にドットナビの1つの要素を格納
+	let dotNavigation = document.getElementsByClassName("nav")[0].getElementsByTagName("li");
+	// 「現在○○枚目のスライドを表示している」というインデックス番号を格納する変数
+	let nowIndex = 0;
+	// 現在表示されている画像とドットナビにクラス名を付ける
 	imageSlide[nowIndex].classList.add("show");
 	dotNavigation[nowIndex].classList.add("current");
-	// アニメーションが終わるタイミングでisChangingのステータスをfalseに
-	slideTimer = setTimeout(function () {
-		isChanging = false;
-	}, 600);
-}
-
-//スライド自動送りを開始する関数
-function startInterval() {
-	Interval = setInterval(function () {
-		let index = nowIndex + 1;
-		if (index > length) {
-			index = 0;
+	// スライドがアニメーション中か判断するフラグ
+	let isChanging = false;
+	// スライド切り替え時に呼び出す関数
+	function sliderSlide(val) {
+		if (isChanging === true) {
+			return false;
 		}
-		sliderSlide(index);
-	}, 4000);
-}
-
-//スライド自動送りを開始
-startInterval();
-
-//画像及びナビにホバーで自動送り停止
-$("#slider").hover(
-	function () { clearInterval(Interval); },
-	function () { startInterval(); }
-);
-
-// ドットナビをクリックした時のイベントを作成
-for (let i = 0; i < dotNavigation.length; i++) {
-	// データ属性のインデックス番号を元にスライドを行う
-	dotNavigation[i].addEventListener("click", function () {
-		let index = Number(this.getAttribute("data-nav-index"));
-		sliderSlide(index);
-	}, false);
-}
-
-// 左矢印のナビをクリックした時のイベント
-document.getElementById("arrow-prev").addEventListener("click", function () {
-	let index = nowIndex - 1;
-	if (index < 0) {
-		index = length;
+		isChanging = true;
+		// 現在表示している画像とナビからクラス名を削除
+		imageSlide[nowIndex].classList.remove("show");
+		dotNavigation[nowIndex].classList.remove("current");
+		nowIndex = val;
+		// 次に表示するスライドとナビにカレントクラスを設定
+		imageSlide[nowIndex].classList.add("show");
+		dotNavigation[nowIndex].classList.add("current");
+		// アニメーションが終わるタイミングでisChangingのステータスをfalseに
+		slideTimer = setTimeout(function () {
+			isChanging = false;
+		}, 600);
 	}
-	sliderSlide(index);
-}, false);
-// 右矢印のナビをクリックした時のイベント
-document.getElementById("arrow-next").addEventListener("click", function () {
-	let index = nowIndex + 1;
-	if (index > length) {
-		index = 0;
+
+	//スライド自動送りを開始する関数
+	function startInterval() {
+		Interval = setInterval(slideRight, 4000);
 	}
-	sliderSlide(index);
-}, false);
+
+	//スライド自動送りを開始
+	startInterval();
+
+	//画像及びナビにホバーで自動送り停止
+	$("#slider").hover(
+		function () { clearInterval(Interval); },
+		function () { startInterval(); }
+	);
+
+	// ドットナビをクリックした時のイベントを作成
+	for (let i = 0; i < dotNavigation.length; i++) {
+		// データ属性のインデックス番号を元にスライドを行う
+		dotNavigation[i].addEventListener("click", function () {
+			let index = Number(this.getAttribute("data-nav-index"));
+			sliderSlide(index);
+		}, false);
+	}
+
+	// 左矢印のナビをクリックした時のイベント
+	$("#arrow-prev").on("click", slideLeft);
+	// 右矢印のナビをクリックした時のイベント
+	$("#arrow-next").on("click", slideRight);
 
 
-// スワイプ
-$(document).ready(function () {
+	// スワイプ
 	// 指が触れたらstartSwipeを実行
 	$("#slider").on("touchstart", startSwipe);
 
@@ -128,9 +108,8 @@ $(document).ready(function () {
 	// 指が離れたらendSwipeを実行
 	$("#slider").on("touchend", endSwipe);
 
-	let moveX, posiX;
 
-
+	var moveX, posiX;
 	// 指が触れた時の処理
 	function startSwipe(event) {
 		// 現在の座標を取得
@@ -156,21 +135,30 @@ $(document).ready(function () {
 	function endSwipe(event) {
 		// 左へ移動していた場合
 		if (moveX == "left") {
-			let index = nowIndex + 1;
-			if (index > length) {
-				index = 0;
-			}
-			sliderSlide(index);
+			slideRight();
 		}
 		// 右へ移動した場合
 		else if (moveX == "right") {
-			let index = nowIndex - 1;
-			if (index < 0) {
-				index = length;
-			}
-			sliderSlide(index);
+			slideLeft();
 		}
+	}
 
+	// 左へスライドする関数
+	function slideLeft() {
+		let index = nowIndex - 1;
+		if (index < 0) {
+			index = length;
+		}
+		sliderSlide(index);
+	}
+
+	// 右へスライドする関数
+	function slideRight() {
+		let index = nowIndex + 1;
+		if (index > length) {
+			index = 0;
+		}
+		sliderSlide(index);
 	}
 
 	//横方向の座標を取得
