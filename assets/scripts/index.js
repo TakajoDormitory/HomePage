698dd5cb44b5bec86fcdd8664d0175353504e6bc
shebang_sh <c19yamamoto@gmail.com>
2023-04-18 17:02:24 +0900

    :bug:fix: fix filename

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -125,7 +125,7 @@ $(function () {
   // slider
   let imgList = [
     "assets/images/OverView.jpg",
-    "assets/images/EntranceCeremony_new.JPG",
+    "assets/images/EntranceCeremony.JPG",
     "assets/images/Firework.JPG",
     "assets/images/soccer.jpg",
   ];
